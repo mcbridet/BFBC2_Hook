6dd0d6acfd992b5e1acb16692b6e57a541efbc12
Marek Grzyb <grzybdev@gmail.com>
2022-06-23 23:34:05 +0200

    Load original dinput8.dll on startup

--- dinput8/dllmain.cpp
+++ dinput8/dllmain.cpp
@@ -1,19 +1,32 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include "dinput8.h"
+
+void LoadSystemLibrary(const char* libraryName)
+{
+    char libraryPath[MAX_PATH];
+
+	GetSystemDirectoryA(libraryPath, MAX_PATH);
+    strcat_s(libraryPath, "\\");
+    strcat_s(libraryPath, libraryName);
+
+    OriginalLibrary = LoadLibraryA(libraryPath);
+
+    if (OriginalLibrary > reinterpret_cast<HMODULE>(31))
+        OriginalFunction = reinterpret_cast<DirectInput8Create_t>(GetProcAddress(OriginalLibrary, "DirectInput8Create"));
+}
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
                      )
 {
-    switch (ul_reason_for_call)
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
     {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
+	    // Load system library so game can use APIs as if it loaded it normally
+        LoadSystemLibrary("dinput8.dll");
     }
+
     return TRUE;
 }
 
