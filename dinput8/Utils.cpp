8ad67ac1f0af52242427841dc0c8b5b824ba7df4
Marek Grzyb <grzybdev@gmail.com>
2022-07-03 22:02:30 +0200

    Added Utils class

---
+++ dinput8/Utils.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "Utils.h"
+
+void Utils::CenterPrint(std::string text, const char* fillChar, bool newLine)
+{
+	using namespace std;
+
+	if (!text.empty())
+		text = " " + text + " ";
+
+	CONSOLE_SCREEN_BUFFER_INFO console_screen_buffer_info;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &console_screen_buffer_info);
+
+	const int columns = console_screen_buffer_info.srWindow.Right - console_screen_buffer_info.srWindow.Left + 1;
+	const int padding_size = (columns / 2) - (text.size() / 2);
+
+	PrintChar(fillChar, padding_size);
+	cout << text;
+	PrintChar(fillChar, padding_size);
+
+	if (newLine)
+		cout << endl;
+}
+
+void Utils::PrintChar(const char* charToPrint, const unsigned int count)
+{
+	using namespace std;
+
+	for (unsigned int i = 0; i < count; i++)
+		cout << charToPrint;
+}
---
+++ dinput8/Utils.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+class Utils
+{
+public:
+    static void CenterPrint(std::string text, const char* fillChar, bool newLine);
+    static void PrintChar(const char* charToPrint, const unsigned int count);
+};
