39f723154e6a0ca16902b7091953c522206ca147
Marek Grzyb <grzybdev@gmail.com>
2022-07-03 22:02:16 +0200

    Added Config class

---
+++ dinput8/Config.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+#include "Config.h"
+
+Config::Config()
+{
+	readConfig();
+}
+
+void Config::readConfig() const
+{
+	// Create an empty property tree object
+	using boost::property_tree::ptree;
+	ptree pt;
+
+	try
+	{
+		read_json("grzybdev.conf", pt);
+	}
+	catch (boost::property_tree::json_parser_error)
+	{
+		// Do nothing in case of file read error
+		// Will use defaults
+	}
+
+	parseConfig(pt);
+}
+
+void Config::parseConfig(boost::property_tree::ptree pt) const
+{
+	// Section - Debug
+	hook->showConsole = pt.get("debug.showConsole", true);
+	hook->createLog = pt.get("debug.createLog", false);
+
+	boost::posix_time::ptime timeLocal = boost::posix_time::second_clock::local_time();
+	boost::posix_time::time_facet* facet = new boost::posix_time::time_facet("%Y-%m-%d_%H-%M-%S");
+
+	std::ostringstream is;
+	is.imbue(std::locale(is.getloc(), facet));
+	is << timeLocal;
+
+	hook->logPath = pt.get("debug.logPath", (boost::format("bfbc2_%1%.log") % is.str()).str());
+	hook->consoleLogLevel = pt.get("debug.logLevelConsole", boost::log::trivial::info);
+	hook->fileLogLevel = pt.get("debug.logLevelFile", boost::log::trivial::debug);
+}
---
+++ dinput8/Config.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+class Config
+{
+public:
+	Config();
+	Config(const Config&) {}
+
+	static Config& getInstance() {
+		static Config* _instance = nullptr;
+
+		if (_instance == nullptr)
+			_instance = new Config();
+
+		return *_instance;
+	}
+
+	struct HookConfig
+	{
+		/* Debug */
+		bool showConsole; // Show debug console
+		bool createLog; // Create a log file
+		std::string logPath; // Log file path
+
+		int consoleLogLevel; // Console message level
+		int fileLogLevel; // File message level
+	};
+
+	HookConfig* hook = new HookConfig();
+
+private:
+	void readConfig() const;
+	void parseConfig(boost::property_tree::ptree pt) const;
+};
