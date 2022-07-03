831d9d38dda42ebd294a8dabc8222b061ae6d191
Marek Grzyb <grzybdev@gmail.com>
2022-07-03 22:04:50 +0200

    Start HookInit in new thread

--- dinput8/dllmain.cpp
+++ dinput8/dllmain.cpp
@@ -1,32 +1,36 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 #include "dinput8.h"
+#include "Hook.h"
 
 void LoadSystemLibrary(const char* libraryName)
 {
-    char libraryPath[MAX_PATH];
+	char libraryPath[MAX_PATH];
 
 	GetSystemDirectoryA(libraryPath, MAX_PATH);
-    strcat_s(libraryPath, "\\");
-    strcat_s(libraryPath, libraryName);
+	strcat_s(libraryPath, "\\");
+	strcat_s(libraryPath, libraryName);
 
-    OriginalLibrary = LoadLibraryA(libraryPath);
+	OriginalLibrary = LoadLibraryA(libraryPath);
 
-    if (OriginalLibrary > reinterpret_cast<HMODULE>(31))
-        OriginalFunction = reinterpret_cast<DirectInput8Create_t>(GetProcAddress(OriginalLibrary, "DirectInput8Create"));
+	if (OriginalLibrary > reinterpret_cast<HMODULE>(31))
+		OriginalFunction = reinterpret_cast<DirectInput8Create_t>(  // NOLINT(clang-diagnostic-cast-function-type)
+			GetProcAddress(OriginalLibrary, "DirectInput8Create"));
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE hModule,
+                      DWORD ul_reason_for_call,
+                      LPVOID lpReserved
+)
 {
-    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
-    {
-	    // Load system library so game can use APIs as if it loaded it normally
-        LoadSystemLibrary("dinput8.dll");
-    }
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+	{
+		// Load system library so game can use APIs as if it loaded it normally
+		LoadSystemLibrary("dinput8.dll");
 
-    return TRUE;
+		// Launch hook code in new thread
+		CreateThread(nullptr, 0, HookInit, nullptr, 0, nullptr);
+	}
+
+	return TRUE;
 }
-
