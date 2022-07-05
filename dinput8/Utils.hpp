b0689580ae6920042fcf7724ca097debec6b7053
Marek Grzyb <grzybdev@gmail.com>
2022-07-05 16:03:57 +0200

    Added Utility functions to find patterns in game memory

--- dinput8/Utils.hpp
+++ dinput8/Utils.hpp
@@ -5,4 +5,8 @@ class Utils
 public:
     static void CenterPrint(std::string text, const char* fillChar, bool newLine);
     static void PrintChar(const char* charToPrint, unsigned int count);
+
+    static DWORD FindPattern(DWORD dwStart, DWORD dwLen, BYTE* pszPatt, const char pszMask[]);
+    static DWORD OffsetToCode(HANDLE hHandle);
+    static DWORD GetSizeOfCode(HANDLE hHandle);
 };
