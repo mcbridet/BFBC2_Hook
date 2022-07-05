941352521fd023c9e6ca8ad7ed3d4fd849fd9603
Marek Grzyb <grzybdev@gmail.com>
2022-07-05 16:57:10 +0200

    Defined _WINSOCK_DEPRECATED_NO_WARNINGS

--- dinput8/framework.hpp
+++ dinput8/framework.hpp
@@ -2,6 +2,7 @@
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 #define _WIN32_WINNT 0x0502				// Target WinXP SP2
+#define _WINSOCK_DEPRECATED_NO_WARNINGS // Disable stupid MS "warnings" a.k.a errors
 
 // Windows Header Files
 #include <Windows.h>
