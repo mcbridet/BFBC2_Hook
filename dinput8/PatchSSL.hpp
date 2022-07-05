c1fcdd52204c4d71b62b23c63ca87bc79625912b
Marek Grzyb <grzybdev@gmail.com>
2022-07-05 16:14:45 +0200

    Added SSL Patch

---
+++ dinput8/PatchSSL.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+class PatchSSL
+{
+public:
+    PatchSSL();
+    PatchSSL(const PatchSSL&) {}
+
+    static PatchSSL& getInstance() {
+        static PatchSSL* _instance = nullptr;
+
+        if (_instance == nullptr)
+            _instance = new PatchSSL();
+
+        return *_instance;
+    }
+
+    bool patchSSLVerification();
+
+private:
+    bool enablePatch;
+
+    DWORD dwCodeSize;
+    DWORD dwCodeOffset;
+    DWORD dwEntryPoint;
+};
