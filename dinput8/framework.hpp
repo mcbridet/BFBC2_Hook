36e25d56bfa2d30347db2d5694f2359662304add
Marek Grzyb <grzybdev@gmail.com>
2022-07-10 11:07:18 +0200

    Added definitions related to proxy

--- dinput8/framework.hpp
+++ dinput8/framework.hpp
@@ -4,6 +4,14 @@
 #define _WIN32_WINNT 0x0502				// Target WinXP SP2
 #define _WINSOCK_DEPRECATED_NO_WARNINGS // Disable stupid MS "warnings" a.k.a errors
 
+#define CLIENT_FESL_PORT 18390
+#define CLIENT_THEATER_PORT 18395
+#define SERVER_FESL_PORT 19021
+#define SERVER_THEATER_PORT 19026
+
+#define PACKET_MAX_LENGTH 8192
+#define HEADER_LENGTH 4
+
 // Windows Header Files
 #include <Windows.h>
 
