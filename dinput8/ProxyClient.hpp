148bc419232b8bc4853e5878c96e5f0af2fce136
Marek Grzyb <grzybdev@gmail.com>
2022-11-13 18:21:10 +0100

    Added udp_send_data for ProxyUDP logging

--- dinput8/ProxyClient.hpp
+++ dinput8/ProxyClient.hpp
@@ -20,5 +20,7 @@ public:
 
     ProxyUDP* theaterCtx;
     web::websockets::client::websocket_callback_client theater_ws;
+
     bool connected_to_theater = false;
+    unsigned char udp_send_data[PACKET_MAX_LENGTH];
 };
